a1d56e2f8740c0ac98fa75f94d4e037222ba6ab9
honeydeepbhatti <honey.deep14@gmail.com>
2024-06-02 21:59:25 -0400

    Basic Changes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -15,6 +15,7 @@ public class BaseTest {
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+<<<<<<< Updated upstream
     }
     @BeforeMethod
     public void launchBrowser(){
@@ -71,3 +72,61 @@ public class BaseTest {
 
     }
 }
+=======
+}
+@BeforeMethod
+public void launchBrowser(){
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+    options.addArguments("--disable-notifications");
+
+    driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.manage().window().maximize();
+
+    navigateToPage();
+
+}
+@AfterMethod
+public void closeBrowser(){
+    driver.quit();
+}
+
+
+public void navigateToPage(){
+
+    driver.get(url);
+
+}
+
+public void provideEmail(String email){
+
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.clear();
+    emailField.sendKeys(email);
+
+}
+
+public void providePassword(String password){
+
+    WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+    passwordField.clear();
+    passwordField.sendKeys(password);
+
+}
+
+public void clickLogin(){
+
+    WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+    loginButton.click();
+
+}
+
+public void logOut(){
+
+    WebElement logoutButton = driver.findElement(By.cssSelector("button[data-title='Log out']"));
+    logoutButton.click();
+
+}
+}
+>>>>>>> Stashed changes
