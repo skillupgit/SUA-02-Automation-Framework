4ea530b7de92a5c006cf74945aa836108f81e525
honeydeepbhatti <honey.deep14@gmail.com>
2024-06-23 16:30:35 -0400

    Updated Lesson 18 Practtice

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,25 +1,115 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
     public void loginEmptyEmailPassword() {
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         String url = "https://demo.koel.dev/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        //driver.quit();
     }
+
+    @Test
+    public void logicSuccess() throws InterruptedException {
+
+        provideEmail("demo@koel.dev");
+        providePassword("demo");
+        clickLogin();
+        //WebElement profileAvatar = driver.findElement(By.cssSelector("a.view-profile img"));
+        //WebElement profileAvatar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a.view-profile img")));
+        WebElement profileAvatar = fluentWait.until
+                (ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a.view-profile img")));
+        Assert.assertTrue(profileAvatar.isDisplayed());
+    }
+
+    @Test (enabled = false)
+    public void incorrectEmail() throws InterruptedException {
+
+        String expectedUrl = "https://demo.koel.dev/";
+        //navigateToPage();
+
+        provideEmail("google@koel.dev");
+        Thread.sleep(1000);
+
+        providePassword("demo");
+        Thread.sleep(1000);
+
+        clickLogin();
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+
+    }
+
+    @Test (enabled = false)
+    public void incorrectPassword() throws InterruptedException {
+        String expectedUrl = "https://demo.koel.dev/";
+
+        //navigateToPage();
+
+
+        provideEmail("demo@koel.dev");
+        Thread.sleep(1000);
+
+
+        providePassword("12345");
+        Thread.sleep(1000);
+
+
+        clickLogin();
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+
+
+    }
+
+
+    @Test(enabled = true, description = "Not Yet due to issue")
+    public void logOutTest() throws InterruptedException {
+
+        String expectedUrl = "https://demo.koel.dev/";
+
+        //navigateToPage();
+
+        provideEmail("demo@koel.dev");
+        Thread.sleep(1000);
+
+        providePassword("demo");
+        Thread.sleep(1000);
+
+        clickLogin();
+        Thread.sleep(1000);
+
+        logOut();
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.getCurrentUrl(),expectedUrl);
+
+    }
+
+    @Test(enabled = false, dataProvider = "NegativeLoginTestData", dataProviderClass = TestDataSets.class)
+    public void negativeLoginTest(String email, String password) throws InterruptedException {
+
+        String expectedUrl = "https://demo.koel.dev/";
+        //navigateToPage();
+
+        provideEmail(email);
+        Thread.sleep(1000);
+
+        providePassword(password);
+        Thread.sleep(1000);
+
+        clickLogin();
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+
+    }
+
 }
